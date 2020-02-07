5c267371694ed939947b53425b72e13aa8a0d16e
KI-RA-CO <kihara4173@icloud.com>
2020-02-07 21:08:02 +0900

    initial_commit

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,10 @@
+class BoardsController < ApplicationController
+
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    
+  end
+end
---
+++ db/migrate/20200207101155_create_boards.rb
@@ -0,0 +1,10 @@
+class CreateBoards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :boards do |t|
+      t.string :title, null: false
+      t.string :content, null: false
+      t.index :title, unique: true
+      t.timestamps
+    end
+  end
+end
