e806c29af5c4c5ed088a9df98c0f7973c1fa41db
KI-RA-CO <kihara4173@icloud.com>
2020-02-08 15:36:13 +0900

    created_homes-view#1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'boards#index'
+  resources :homes, only:[:index]
   resources :boards, only:[:index,:new, :create, :show, :edit, :update, :destroy]
 end
