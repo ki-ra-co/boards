fb976718729ae0d53309d84bf6f270a3efbd1ddd
KI-RA-CO <kihara4173@icloud.com>
2020-02-08 16:25:56 +0900

    create_user-view#1
    ログイン画面とサインアップ画面のビューを実装した

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_214449) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_214449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
