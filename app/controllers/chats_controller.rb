deebd4209e6ccdb8821ba240a617eb2554d0608a
KI-RA-CO <kihara4173@icloud.com>
2020-02-09 20:23:08 +0900

    copy_form_chat-view
    chatspaceアプリからチャット画面のコメント送信フォームをコピーしてきた

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,4 +2,13 @@ class ChatsController < ApplicationController
   def index
     @chats = Chat.all
   end
+
+  def create 
+    @chat = @board.chats.new(chat_params)
+  end
+  private
+
+  def chat_params
+    params.require(:chat).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
