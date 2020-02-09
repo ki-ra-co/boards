9fd785071828c5a16924f36f66374aa11546381e
KI-RA-CO <kihara4173@icloud.com>
2020-02-09 21:23:30 +0900

    add_chats-controller_action
    chatコントローラーのindexとcreateアクションにメソッドを定義して、ひとまずはboard-showページからchat-indexページに飛べるようになった

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,14 +1,23 @@
 class ChatsController < ApplicationController
+  before_action :set_board
+
   def index
-    @chats = Chat.all
+    @chat = Chat.new
+    @chats = @board.content
   end
 
-  def create 
+  def create
     @chat = @board.chats.new(chat_params)
   end
+
+
   private
 
   def chat_params
     params.require(:chat).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_board
+    @board = Board.find(params[:board_id])
   end
 end
