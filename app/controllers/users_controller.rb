f82e011726a29ce3adecff9e68251dc2224b7882
KI-RA-CO <kihara4173@icloud.com>
2020-02-10 00:47:29 +0900

    created_home-view#2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
 
-  
-    
-  
-
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'boards#index'
+  root to: 'homes#index'
   resources :homes, only:[:index]
-  resources :boards, only:[:index,:new, :create, :show, :edit, :update, :destroy] do
-    resources :chats, only:[:index, :create]
-  end
+    resources :boards, only:[:index,:new, :create, :show, :edit, :update, :destroy] do
+      resources :chats, only:[:index, :create]
+    end
   resources :users, only: [:edit, :update]
 end
