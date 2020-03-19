250a251a43a24a2cf32cee72334ff012d6ff0100
KI-RA-CO <kihara4173@icloud.com>
2020-03-20 00:16:13 +0900

    Adjustment　login&signup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#index'
+  root to: 'boards#index'
   resources :users, only: [:edit, :update]
-  resources :homes, only:[:index]
-    resources :boards, only:[:index,:new, :create, :show, :edit, :update, :destroy] do
-      resources :chats, only:[:index, :create]
-    end
+  resources :boards, only:[:index,:new, :create, :show, :edit, :update, :destroy] do
+    resources :chats, only:[:index, :create]
+  end
 end
